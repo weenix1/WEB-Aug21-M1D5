4a0042f67495ba79b32f4e10e2ea08fedb184f28
Weenix Star <60514231+weenix1@users.noreply.github.com>
2021-08-13 16:20:37 +0200

    Create function-M1D5.js

---
+++ function-M1D5.js
@@ -0,0 +1,183 @@
+/*
+ASSIGNMENT RULES
+- All the answers must be written in JavaScript
+- You can ask for help, reach the Teaching Assistants if needed
+- The solution must be available for the tutors by the end of the day (5PM CET)
+- You can Google / use StackOverflow BUT we suggest you to use just the material provided
+*/
+
+/* EXERCISE 1
+Write a function "area" which receives 2 parameters (l1,l2) and calculates the area of the associated rectangle.
+*/
+
+const rectangle = function (length, width) {
+  let area = length * width;
+
+  return area;
+};
+
+//let areaOfRectangle = rectangle(11, 12);
+console.log(`The area of a rectangle is :${rectangle(11, 12)}`);
+//console.log("The area of a rectangle is :" + areaOfRectangle);
+
+/* EXERCISE 2
+Write a function "crazySum" which receives two integers. It should return the sum of those two values, but if the two values are the same then it should return their sum multiplied by 3.
+*/
+
+function crazySum(a, b) {
+  let sum;
+  if (a !== b) {
+    sum = (a + b) * 3;
+  } else {
+    sum = a + b;
+  }
+  return sum;
+}
+
+console.log(`The sum of two integers a and b is: ${crazySum(3, 3)}`);
+
+/* EXERCISE 3
+Write a function "crazyDiff" that computes the absolute difference between a given number and 19. 
+It should return triple their absolute difference if the given number is greater than 19.
+*/
+
+function crazyDiff(max, min) {
+  let absValue;
+  if (min < 19) {
+    absValue = max - min;
+  } else {
+    absValue = (max - min) * (max - min) * (max - min);
+  }
+  return absValue;
+}
+
+console.log(
+  `The absolute difference between this numbers: ${crazyDiff(19, 10)}`
+);
+/* EXERCISE 4
+Write a function "boundary" which accept an integer n and returns true if n is within 20 and 100 (included) or if it's equal to 400.
+*/
+
+/* WRITE YOUR CODE HERE */
+
+function boundary(n) {
+  let range = [1, 2];
+}
+
+/* EXERCISE 5
+Write a function "strivify" which accepts a string.
+It should add the word "Strive" in front of the given string, but if the given string already begins with "Strive", then it should just return the original string.
+*/
+
+/* WRITE YOUR CODE HERE */
+
+/* EXERCISE 6
+Write a function "check3and7" which accepts a positive number and check if it is a multiple of 3 or a multiple of 7.
+HINT: Module Operator
+*/
+
+/* WRITE YOUR CODE HERE */
+
+/* EXERCISE 7
+Write a function "reverseString" to programmatically reverse a given string (es.: Strive => evirtS).
+*/
+
+/* WRITE YOUR CODE HERE */
+
+/* EXERCISE 8
+Write a function "upperFirst" to capitalize the first letter of each word of a given string passed as a parameter.
+*/
+
+/* WRITE YOUR CODE HERE */
+
+/* EXERCISE 9
+Write a function "cutString" to create a new string without the first and last character of a given string.
+*/
+
+/* WRITE YOUR CODE HERE */
+
+/* EXERCISE 10
+Write a function "giveMeRandom" which accepts a number n and returns an array containing n random numbers between 0 and 10.
+*/
+
+/* WRITE YOUR CODE HERE */
+
+/* WHEN YOU ARE FINISHED
+Commit and push the code to your personal GitHub repository and share the link to your commit in Eduflow.
+*/
+
+/* EXERCISE 11
+Write a function "checkArray" which receives an array of random numbers (created with giveMeRandom) and prints, for each item, whether it's bigger than 5.
+The function returns the sum of the numbers bigger than 5.
+*/
+
+/* WRITE YOUR CODE HERE */
+
+/* EXERCISE 12
+In your eCommerce you have an array of objects called shoppingCart. In this array you have a number of objects with a price, a name, an id and the quantity to be shipped.
+Create a function "shippingCartTotal" which calculates the total due to the shop.
+*/
+
+/* WRITE YOUR CODE HERE */
+
+/* EXERCISE 13
+In your eCommerce you have an array of objects called shoppingCart. In this array you have a number of objects with a price, a name, an id and the quantity to be shipped.
+Create a function "addToShoppingCart" which receives a new object, adds it to shoppingCart and returns the total number of items in the shoppingCart.
+*/
+
+/* WRITE YOUR CODE HERE */
+
+/* EXERCISE 14
+In your eCommerce you have an array of objects called shoppingCart. In this array you have a number of objects with a price, a name, an id and the quantity to be shipped.
+Create a function "maxShoppingCart" which receives the shoppingCart array and returns the most expensive item in the array.
+*/
+
+/* WRITE YOUR CODE HERE */
+
+/* EXERCISE 15
+In your eCommerce you have an array of objects called shoppingCart. In this array you have a number of objects with a price, a name, an id and the quantity to be shipped.
+Create a function "latestShoppingCart" which receives the shoppingCart array and returns the last item.
+*/
+
+/* WRITE YOUR CODE HERE */
+
+/* EXERCISE 16
+Create a function "loopUntil" which receives an integer x between 0 and 9.
+The function loops and prints a random number between 0 and 9 until the random number is bigger than x for three times in a row.
+*/
+
+/* WRITE YOUR CODE HERE */
+
+/* EXERCISE 17
+Write a function "average" which receives an array and return the average value. The function automatically skips non-numeric entries in the array.
+*/
+
+/* WRITE YOUR CODE HERE */
+
+/* EXERCISE 18
+Write a function "longest" to find the longest string from an given array of strings.
+*/
+
+/* WRITE YOUR CODE HERE */
+
+/* EXERCISE 19
+Write a function to create a very simple anti spam filter for your mailbox. The function takes a string emailContent, and returns a boolean.
+Check if the email is valid using string methods. The email (in this example) is valid if the words SPAM and SCAM does not appear.
+*/
+
+/* WRITE YOUR CODE HERE */
+
+/* EXERCISE 20
+Write a function that receives a date d as parameter and calculates the number of days passes since the d.
+*/
+
+/* WRITE YOUR CODE HERE */
+
+/* EXERCISE 21
+Write a function "matrixGenerator" that receives x and y as parameter. The result should be a matrix of x times y with, as value, the index of the position.
+Ex.: X = 3, Y = 2
+["00","01","02"
+"10","11","12"]
+*/
+
+/* WRITE YOUR CODE HERE */
